601715a9ed6b409c3dbb13531e5675a660196e0b
Oleksij Lupandin <oleksii_lupandin@gmail.com>
2017-01-27 12:19:31 +0100

    Renamed tests

--- src/test/java/com/ubs/opsit/interviews/berlinclock/BerlinClockTest.java
+++ src/test/java/com/ubs/opsit/interviews/berlinclock/BerlinClockTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static com.ubs.opsit.interviews.berlinclock.BerlinClock.NEWLINE;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class BerlinClockTest {
 
@@ -18,7 +17,7 @@ public class BerlinClockTest {
     private final int MINUTES2 = 4;
 
     @Test
-    public void testEvenSecond() {
+    public void secondLampShouldBeYellowAtEvenSecond() {
         // arrange
         final String timeWithEvenSeconds = "00:00:02";
 
@@ -31,7 +30,7 @@ public class BerlinClockTest {
     }
 
     @Test
-    public void testOddSecond() {
+    public void secondLampShouldBeOffAtOddSecond() {
         // arrange
         final String timeWithOddSeconds = "00:00:05";
 
@@ -44,20 +43,20 @@ public class BerlinClockTest {
     }
 
     @Test
-    public void testFirstHourLine() {
+    public void firstHourLineShouldBePartiallyFilledBetween5And19Hours() {
         // arrange
-        final String timeWithMoreThan4Hr = "14:00:00";
+        final String timeWithMoreThan4AndLessThan20Hr = "14:00:00";
 
         // act
-        final String output = berlinClock.convertTime(timeWithMoreThan4Hr);
+        final String output
+                = berlinClock.convertTime(timeWithMoreThan4AndLessThan20Hr);
 
-        // assert
-        final String[] lines = output.split(NEWLINE);
-        assertThat(lines[HOURS1]).isEqualTo("RROO");
+         // assert
+        assertThat(extractLine(output, HOURS1)).isEqualTo("RROO");
     }
 
     @Test
-    public void testSecondHourLine() {
+    public void secondHourLineShouldBePartiallyFilledAtNoMultiple5Hours() {
         // arrange
         final String timeWithNoMultiple5Hr = "21:00:00";
 
@@ -65,12 +64,11 @@ public class BerlinClockTest {
         final String output = berlinClock.convertTime(timeWithNoMultiple5Hr);
 
         // assert
-        final String[] lines = output.split(NEWLINE);
-        assertThat(lines[HOURS2]).isEqualTo("ROOO");
+        assertThat(extractLine(output, HOURS2)).isEqualTo("ROOO");
     }
 
     @Test
-    public void testYellowPlacesInFirstMinuteLine() {
+    public void firstMinuteLineShouldBePartiallyFilledYellowAtLessThan15Minutes() {
         // arrange
         final String timeWithMultiple5Min = "18:10:27";
 
@@ -78,12 +76,11 @@ public class BerlinClockTest {
         final String output = berlinClock.convertTime(timeWithMultiple5Min);
 
         // assert
-        final String[] lines = output.split(NEWLINE);
-        assertThat(lines[MINUTES1]).isEqualTo("YYOOOOOOOOO");
+        assertThat(extractLine(output, MINUTES1)).isEqualTo("YYOOOOOOOOO");
     }
 
     @Test
-    public void testRedPlacesInFirstMinuteLine() {
+    public void firstMinuteLineShouldHaveRedLampsForMultiple15Minutes() {
         // arrange
         final String timeWithMultiple15Min = "19:30:40";
 
@@ -91,12 +88,11 @@ public class BerlinClockTest {
         final String output = berlinClock.convertTime(timeWithMultiple15Min);
 
         // assert
-        final String[] lines = output.split(NEWLINE);
-        assertThat(lines[MINUTES1]).isEqualTo("YYRYYROOOOO");
+        assertThat(extractLine(output, MINUTES1)).isEqualTo("YYRYYROOOOO");
     }
 
     @Test
-    public void testSecondMinuteLine() {
+    public void secondMinuteLineShoulBePartiallyFilledAtNoMultiple5Minutes() {
         // arrange
         final String timeWithNoMultiple5Min = "20:06:07";
 
@@ -104,8 +100,7 @@ public class BerlinClockTest {
         final String output = berlinClock.convertTime(timeWithNoMultiple5Min);
 
         // assert
-        final String[] lines = output.split(NEWLINE);
-        assertThat(lines[MINUTES2]).isEqualTo("YOOO");
+        assertThat(extractLine(output, MINUTES2)).isEqualTo("YOOO");
     }
 
     @Test
@@ -161,6 +156,12 @@ public class BerlinClockTest {
 
         // assert
         assertThat(output).isEqualTo(expectedBerlinClockState);
+    }
+
+    private String extractLine(final String output, final int lineIndex) {
+        final String[] lines = output.split(NEWLINE);
+        final String line = lines[lineIndex];
+        return line;
     }
 
     private String layoutAsString(
